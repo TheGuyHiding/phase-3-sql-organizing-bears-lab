f4d5d59637c96076213fcb31cd19176de64410b5
Luis Ibarra <luis9609@gmail.com>
2022-06-30 17:28:46 -0600

    commit tree ea985bdce96ff1de5837fdc5f3ccde8ad28b31f2

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 12, "M", "black", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 8, "F", "dark brown", "calm", 1);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 22, "M", "black", "loud", 0);
+INSERT INTO bears VALUES (5,"Melissa", 13, "F", "light brown", "friendly", 1);
+INSERT INTO bears VALUES (6,"Grinch", 29, "M", "brown", "tired", 0);
+INSERT INTO bears VALUES (7,"Wendy", 2, "F", "brown", "playful", 1);
+INSERT INTO bears VALUES (8,NULL, 3, "M", "panda", "playful", 1);
+
